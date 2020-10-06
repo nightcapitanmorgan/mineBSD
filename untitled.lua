f483beee6e73b3181b39f6f1111894e1b080d26d
nightcapitanmorgan <72296922+nightcapitanmorgan@users.noreply.github.com>
2020-10-06 17:12:31 +0300

    Add files via upload

---
+++ untitled.lua
@@ -0,0 +1,9 @@
+local component = require("component")
+local event = require("event")
+local gpu = component.gpu 
+term.clear() 
+term.setcursorpos(1, 2)
+print("test")
+local dispname = read() 
+print("OK")
+local realname = read()
